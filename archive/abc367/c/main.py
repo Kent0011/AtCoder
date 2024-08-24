28187dee7069fc7ea46a9bf94f9676ca8b9ce9f5
Kento Yamamoto <key7135g@gmail.com>
2024-08-24 13:27:04 +0900

    abc367

--- archive/abc367/c/main.py
+++ archive/abc367/c/main.py
@@ -1,9 +1,12 @@
 import numpy as np
 import math 
 import sys
-from functools import lru_cache
+from functools import lru_cache 
+import itertools
 
 # n = int(input())
-# n,k = map(int,input().split())
-# A = list(map(int,input().split()))
+n,k = map(int,input().split())
+R = list(map(int,input().split()))
 
+for i in itertools.product(*(range(1, r + 1) for r in R)):
+    if sum(i)%k==0: print(*i)
--- init/template.py
+++ init/template.py
@@ -2,6 +2,7 @@ import numpy as np
 import math 
 import sys
 from functools import lru_cache
+import itertools
 
 # n = int(input())
 # n,k = map(int,input().split())
