e6057b6ba26da9fa926711ec6cfe352ecdb25c78
Kento Yamamoto <key7135g@gmail.com>
2024-09-24 19:03:16 +0900

    typical90

--- typical90/007/main.py
+++ typical90/007/main.py
@@ -4,7 +4,37 @@ import sys
 from functools import lru_cache
 import itertools
 
-# n = int(input())
-# n,k = map(int,input().split())
-# A = list(map(int,input().split()))
 
+
+n = int(input())
+# n,k = map(int,input().split())
+A = list(map(int,input().split()))
+A.sort()
+
+q = int(input())
+
+def is_ok(b: int, arg: int) -> bool:
+    # 条件を満たすかどうか？問題ごとに定義
+    return arg<n and b>=A[arg] or arg<0
+
+
+def meguru_bisect(b, ng, ok):
+
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(b,mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+for _ in range(q):
+    b = int(input())
+    
+    ans = meguru_bisect(b,n,-1)
+    if ans >=n-1:
+        print(abs(b-A[ans]))
+    else:
+        print(min(abs(b-A[ans]),abs(b-A[ans+1])))
+    
--- typical90/008/main.py
+++ typical90/008/main.py
@@ -4,7 +4,14 @@ import sys
 from functools import lru_cache
 import itertools
 
-# n = int(input())
+n = int(input())
+S = str(input())
 # n,k = map(int,input().split())
 # A = list(map(int,input().split()))
+
+S_list = []
+
+for s in S:
+    if s == 'a'or't'or'c'or'o'or'd'or'e'or'r':
+        S_list.append(s)
 
