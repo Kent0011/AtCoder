006cf24698c9efecb291e5ae7b06c2b6706cc39d
Kento Yamamoto <key7135g@gmail.com>
2024-09-28 16:52:56 +0900

    typical90

--- typical90/010/main.py
+++ typical90/010/main.py
@@ -4,7 +4,24 @@ import sys
 from functools import lru_cache
 import itertools
 
-# n = int(input())
+n = int(input())
 # n,k = map(int,input().split())
 # A = list(map(int,input().split()))
 
+C_1,C_2 = [0], [0]
+
+for i in range(n):
+    c,p = map(int,input().split())
+    if c==1:
+        C_1.append(C_1[-1]+p)
+        C_2.append(C_2[-1])
+    if c==2:
+        C_2.append(C_2[-1]+p)
+        C_1.append(C_1[-1])
+        
+q = int(input())
+
+for i in range(q):
+    l,r = map(int,input().split())
+    ans = [C_1[r]-C_1[l-1], C_2[r]-C_2[l-1]]
+    print(*ans)
