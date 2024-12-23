56169ca3561b4eb7f0679853269003ace01d22f7
Kento Yamamoto <key7135g@gmail.com>
2024-12-23 22:25:10 +0900

    update template

--- init/template.py
+++ init/template.py
@@ -1,10 +1,15 @@
 import numpy as np
+import pandas as pd
+import itertools
 import math 
 import sys
 from functools import lru_cache
-import itertools
+import bisect
 
-# n = int(input())
-# n,k = map(int,input().split())
-# A = list(map(int,input().split()))
 
+if __name__ == "__main__":
+    ...
+    # n = int(input())
+    # n,k = map(int,input().split())
+    # A = list(map(int,input().split()))
+    # S = list(str(input()))
