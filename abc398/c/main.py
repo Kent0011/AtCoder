575ecced18e5302e5ebba50822050d1f90f8372f
Kento Yamamoto <key7135g@gmail.com>
2025-03-26 21:15:05 +0900

    abc398

--- abc398/c/main.py
+++ abc398/c/main.py
@@ -11,7 +11,7 @@ from collections import deque
 
 def main():
     # N (整数)
-    # n = int(input())
+    n = int(input())
     
     # N K (整数)
     # n,k = map(int,input().split())
@@ -23,7 +23,7 @@ def main():
     # w,h = map(int,input().split())
     
     # A1 A2 A3 ... An (整数)
-    # A = list(map(int,input().split()))
+    A = list(map(int,input().split()))
     
     # B1 B2 B3 ... Bn (整数)
     # B = list(map(int,input().split()))
@@ -40,7 +40,23 @@ def main():
     #     A.append(list(map(int,input().split())))
     ...
     
+    uniq = set()
+    notuni = set()
     
+    for i in range(n):
+        if A[i] not in uniq and A[i] not in notuni:
+            uniq.add(A[i])
+        elif A[i] in uniq:
+            uniq.remove(A[i])
+            notuni.add(A[i])
+    
+    if len(uniq) == 0:
+        print(-1)
+        return
+    
+    a = max(uniq)
+    
+    print(A.index(a)+1)
     
 
 if __name__ == "__main__": 
--- abc398/d/main.py
+++ abc398/d/main.py
@@ -20,7 +20,7 @@ def main():
     # n,m = map(int,input().split())
     
     # W H (整数)
-    # w,h = map(int,input().split())
+    w,r,c = map(int,input().split())
     
     # A1 A2 A3 ... An (整数)
     # A = list(map(int,input().split()))
@@ -29,7 +29,7 @@ def main():
     # B = list(map(int,input().split()))
     
     # S (文字列)
-    # S = list(str(input()))
+    S = list(str(input()))
     
     # S1 S2 S3 ... Sn (文字列)
     # S = list(map(str,input().split()))
@@ -40,7 +40,39 @@ def main():
     #     A.append(list(map(int,input().split())))
     ...
     
+    fgrid = set()
+    fire = [0,0]
+    ans = []
     
+    fgrid.add((0,0))
+    
+    for s in S:
+        
+        if s == 'S':
+            r-=1
+            fire[0]-=1
+            fgrid.add(tuple(fire))
+        elif s == 'E':
+            c-=1
+            fire[1]-=1
+            fgrid.add(tuple(fire))
+        elif s == 'N':
+            r+=1
+            fire[0]+=1
+            fgrid.add(tuple(fire))
+        elif s == 'W':
+            c+=1
+            fire[1]+=1
+            fgrid.add(tuple(fire))
+        
+        if (r,c) in fgrid:
+            ans.append("1")
+        else:
+            ans.append("0")
+        
+        
+    
+    print("".join(ans))
     
 
 if __name__ == "__main__": 
