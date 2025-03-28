51b801b7564a8366cd3d86b01b088a03f239fab7
Kento Yamamoto <key7135g@gmail.com>
2025-03-28 20:14:52 +0900

    typical90

--- typical90/008/main.py
+++ typical90/008/main.py
@@ -9,9 +9,30 @@ S = str(input())
 # n,k = map(int,input().split())
 # A = list(map(int,input().split()))
 
-S_list = []
+S_list = {}
+
+S_list['a'] = 0
+S_list['t'] = 0
+S_list['c'] = 0
+S_list['o'] = 0
+S_list['d'] = 0
+S_list['e'] = 0
+S_list['r'] = 0
 
 for s in S:
-    if s == 'a'or't'or'c'or'o'or'd'or'e'or'r':
-        S_list.append(s)
+    if s == 'a':
+        S_list['a'] += 1
+    elif s == 't':
+        S_list['t'] += S_list['a']
+    elif s == 'c':
+        S_list['c'] += S_list['t']
+    elif s == 'o':
+        S_list['o'] += S_list['c']
+    elif s == 'd':
+        S_list['d'] += S_list['o']
+    elif s == 'e':
+        S_list['e'] += S_list['d']
+    elif s == 'r':
+        S_list['r'] += S_list['e']
 
+print(S_list['r']%(10**9+7))
