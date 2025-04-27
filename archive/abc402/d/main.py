36642ac4a24fa10d71c88ede65e2dbb2041ede01
Kento Yamamoto <key7135g@gmail.com>
2025-04-27 19:49:52 +0900

    abc402

---
+++ archive/abc402/d/main.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import itertools
+import math 
+import sys
+from functools import lru_cache
+import bisect
+import heapq
+from collections import deque, defaultdict
+
+
+def main():
+    # N (整数)
+    # n = int(input())
+    
+    # N K (整数)
+    # n,k = map(int,input().split())
+    
+    # N M (整数)
+    n,m = map(int,input().split())
+    
+    # W H (整数)
+    # w,h = map(int,input().split())
+    
+    # A1 A2 A3 ... An (整数)
+    # A = list(map(int,input().split()))
+    
+    # B1 B2 B3 ... Bn (整数)
+    # B = list(map(int,input().split()))
+    
+    # S (文字列)
+    # S = list(str(input()))
+    
+    # S1 S2 S3 ... Sn (文字列)
+    # S = list(map(str,input().split()))
+    
+    # A (二次元配列)
+    A = []
+    for _ in range(m):
+        A.append(list(map(int,input().split())))
+        A[-1][0] -= 1
+        A[-1][1] -= 1
+    
+    MOD = n/2
+    arr = defaultdict(int)
+    ans = 0
+    for i in range(m):
+        center = (A[i][0]+A[i][1])/2
+        arr[center%MOD] += 1
+        ans += i-arr[center%MOD]+1
+        
+    print(ans)
+if __name__ == "__main__": 
+    main()
